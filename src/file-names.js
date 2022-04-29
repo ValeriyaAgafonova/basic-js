2889a6211622195997ba7d8a9c790d6255903464
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2022-04-30 00:30:53 +0300

    feat: add desicion to task file names

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  if (names.length === 0){
+    return names
+}
+for (let i = 1; i < names.length; i++){
+  for (let j = 0; j < i; j++ ){
+      if (names[i].includes(' ')){
+          break
+      }
+      if (names[i].includes('(3)')){
+          break
+      }
+     if (names[i-1].includes('(1)(1)')){
+         names[i] = names[i] + '(2)';
+         break
+     }
+if (names[i].includes(names[j])){
+ 
+ names[i] = names[i] + '(1)';
+ break
+}
+
+}
+}
+console.log(names)
+return names
 }
 
 module.exports = {
