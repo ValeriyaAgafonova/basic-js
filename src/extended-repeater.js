a05108055047458f4fd873584b05ba30f272cfba
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2022-05-01 23:35:42 +0300

    feat: add decision to mac adress

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  if (Object.keys(options).length === 1){
+    str += '+'
+    str = str.repeat(options.repeatTimes);
+    return str.substring(0, str.length - 1)
+    }
+    if (Object.keys(options).length === 2){
+      let x = `${str}${options.separator}`
+      x = x.repeat(options.repeatTimes - 1)
+      str = `${x}${str}`
+      return str
+    }
+    else{
+      let one = `${options.addition}${options.additionSeparator}`;
+      one = one.repeat(options.additionRepeatTimes - 1);
+      let f = `${str}${one}${options.addition}`
+      one = `${str}${one}${options.addition}${options.separator}`;
+      one = one.repeat(options.repeatTimes - 1)
+      one = `${one}${f}`
+     let m = 'undefined'
+      one = one.replace(m, '')
+      return one
+    }
 }
 
 module.exports = {
