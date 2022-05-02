d4b5a4e2373af45a8db61ef830ec819a116463b6
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2022-05-02 20:50:16 +0300

    feat: add decision

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
+
 const chainMaker = {
+ arr: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.arr.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+ this.arr.push(`( ${value} )`)
+ console.log(this.arr)
+ return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    
+    if (position < 1 || this.arr.length < position || typeof position !== 'number'){
+      throw new Error("You can't remove incorrect link!" )
+    }
+    else{
+      this.arr.splice(position - 1, 1)
+      return this
+    }
+    
+   
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse()
+    return this
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.arr.join('~~')
+    this.arr = []
+    return result
   }
 };
 
